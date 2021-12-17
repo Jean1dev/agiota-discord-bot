820ff9e6fc5f318abc78e52d2ae2ac8e0aedb4d5
Jean <jeanlucafp@gmail.com>
2021-12-17 19:11:39 -0300

    work

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
-  B3_API_KEY: process.env.B3_API_KEY
+  B3_API_KEY: process.env.B3_API_KEY,
+  MAILGUN_API_KEY: process.env.MAILGUN_API_KEY
 }
---
+++ src/services/GerarPDF.js
@@ -0,0 +1,40 @@
+/**
+ * @docs http://pdfkit.org/docs/text.html#lists
+ *
+ */
+
+const PDFDocument = require('pdfkit')
+const fs = require('fs')
+const path = require('path')
+const { randomUUID } = require('crypto')
+
+function criarPDFRetornarCaminho(itens) {
+  const doc = new PDFDocument()
+  const filename = `${randomUUID()}-output.pdf`
+  doc.pipe(fs.createWriteStream(filename))
+
+  doc.image(path.resolve(__dirname, 'assets', 'mamaco.jpg'), {
+    fit: [250, 300],
+    align: 'right',
+    valign: 'right'
+  })
+
+  doc.text(`Relatorios de dividas.`, {
+    width: 410,
+    align: 'center'
+  })
+
+  doc.list(itens)
+
+  doc
+    .addPage()
+    .fillColor('blue')
+    .text('Estamos melhorando', 100, 100)
+    .underline(100, 100, 160, 27, { color: '#0000FF' })
+
+  doc.end()
+
+  return path.resolve(__dirname, filename)
+}
+
+module.exports = criarPDFRetornarCaminho
