b0a97ddd0ada78c09cd670162f88feb7bfd427af
Jeanluca <jeanlucafp@gmail.com>
2024-12-23 13:41:43 -0300

    Update index.js

--- src/handlers/dm/index.js
+++ src/handlers/dm/index.js
@@ -1,6 +1,16 @@
 const { CHAT_GERAL } = require("../../discord-constants")
+const { runQuizTask } = require("../../services")
+
+function dmQuiz() {
+  runQuizTask()
+}
 
 module.exports = (message, client) => {
+  if (message.content === 'quiz') {
+    setTimeout(dmQuiz, 2000)
+    return
+  }
+
   const channel = client.channels.cache.find(channel => channel.name === CHAT_GERAL)
   if (channel && channel.send) {
     setTimeout(() => {
