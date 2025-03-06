86658ff683b18510b355b81169870f97cd0d62bf
Jeanluca <jeanlucafp@gmail.com>
2025-03-06 09:32:40 -0300

    Add crypto opportunity checking command and refactor related handlers

--- src/handlers/b3/atualizar-cotacao-carteira.js
+++ src/handlers/b3/atualizar-cotacao-carteira.js
@@ -1,12 +1,5 @@
 const axios = require('axios')
 const puppeteer = require('puppeteer');
-const {
-    gerarRankingExchanges,
-    getMediaSpread,
-    getRankingExchanges,
-    limparEstatisticas,
-    enviarMensagemAvisoCrypto
-} = require('../../services/cryptoArbitrageService');
 
 const baseUrl = 'https://carteira-14bc707a7fab.herokuapp.com/admin'
 
@@ -55,38 +48,9 @@ function deleteMessageAfterTime(message) {
     setTimeout(() => message.delete(), 10000)
 }
 
-function evidenciarRankingExchanges(exchangesRanking) {
-    const evidenciaCompra = exchangesRanking
-        .filter(ex => ex.type === 'buy')
-        .map(ex => `${ex.exchange_name} - Quantidade de oportunidades ${ex.count}%`)
-        .join('\n')
-
-    const evidenciaVenda = exchangesRanking
-        .filter(ex => ex.type === 'sell')
-        .map(ex => `${ex.exchange_name} - Quantidade de oportunidades ${ex.count}%`)
-        .join('\n')
-
-    enviarMensagemAvisoCrypto(`Ranking de exchanges para compra:\n${evidenciaCompra}\n`)
-    enviarMensagemAvisoCrypto(`Ranking de exchanges para venda:\n${evidenciaVenda}\n`)
-}
-
-async function cryptoHandles() {
-    gerarRankingExchanges()
-    getMediaSpread()
-
-    setTimeout(async () => {
-        getRankingExchanges()
-            .then(data => {
-                evidenciarRankingExchanges(data)
-                setTimeout(limparEstatisticas, 15000)
-            })
-    }, 15000)
-}
-
 module.exports = async message => {
     message.reply('momentinho vou verificar').then(deleteMessageAfterTime)
 
-    await cryptoHandles()
     await axios.default.post(`${baseUrl}`)
 
     const { data: listaAtivos } = await axios.default.get(`${baseUrl}/ativos-sem-image`)
