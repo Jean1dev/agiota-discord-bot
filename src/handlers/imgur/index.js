4d777a93772fba7d900542bb9206737f2f4c3cdc
Jeanluca <jeanlucafp@gmail.com>
2025-05-30 08:56:16 -0300

    refactor: remove randomstring dependency and simplify link generation in imgur handler
    - Eliminated the use of randomstring for generating random image links.
    - Updated the link generation logic to use a simpler approach with Picsum API.
    - Removed related code for link validation and random string generation from imgur handler.
    - Updated package.json and package-lock.json to reflect the removal of randomstring.

--- src/handlers/imgur/index.js
+++ src/handlers/imgur/index.js
@@ -1,41 +1,5 @@
-const axios = require('axios');
-const randomstring = require('randomstring');
-
 module.exports = async message => {
-  const gerar_link_aleatorio = () => {
-    const letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let link_aleatorio = 'https://i.imgur.com/';
-    for (let _ = 0; _ < 5; _++) {
-      link_aleatorio += randomstring.generate({
-        length: 1,
-        charset: letras,
-      });
-    }
-    link_aleatorio += '.jpg';
-    return link_aleatorio;
-  };
-
-  const validar_link = async (link) => {
-    try {
-      const response = await axios.get(link);
-      if (response.request.res.responseUrl !== "https://i.imgur.com/removed.png") {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (error) {
-      return false;
-    }
-  };
-
-  const links_aleatorios = [];
-  while (links_aleatorios.length < 5) {
-    const link = gerar_link_aleatorio();
-    const isValid = await validar_link(link);
-    if (isValid) {
-      links_aleatorios.push(link);
-    }
-  }
+  const links_aleatorios = Array.from({ length: 5 }, (_, i) => `https://picsum.photos/seed/${Date.now()}${i}/600/400`);
 
   links_aleatorios.forEach((link, index) => {
     setTimeout(() => message.channel.send(link), index * 1000)
